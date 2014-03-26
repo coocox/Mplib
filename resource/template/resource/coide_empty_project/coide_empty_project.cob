cf0dbc3150b674cbfbf23c51c068a3c4d1291c7d
xinyun <xinyun@coocox.com>
2014-03-26 11:51:25 +0800

    Add mlib doc

--- resource/template/resource/coide_empty_project/coide_empty_project.cob
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<Project>
-  <Device>
-    <Manufacturer id="Holtek" name="Holtek"/>
-    <Chip id="HT32F1755" name="HT32F1755"/>
-  </Device>
-  <Components/>
-  <ExcludedFiles/>
-</Project>
